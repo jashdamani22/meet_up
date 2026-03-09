02c0ee2b53e04f478cdcf548282feabe658ec13f
Philipp Geiger <hello@philipp-geiger.ch>
2026-03-09 12:08:54 +0000

    Added station coordinate getter

--- app/services/tfl_requests.py
+++ app/services/tfl_requests.py
@@ -5,10 +5,10 @@ from datetime import datetime, timezone
 class TfL_Request:
     def __init__(self):
         # Initialize cache dictionaries. These will help avoid unecessary API calls
-        self.tube_status_cache = {}
-        self.tube_closures_cache = {}
-        self.station_coords_cache = {}
-        self.journey_times_cache = {}
+        self._tube_status_cache = {}
+        self._tube_closures_cache = {}
+        self._station_coords_cache = {}
+        self._journey_times_cache = {}
 
     def _is_cached(self, cache_dict: dict, cache_name: str) -> bool:
         # Check that the cache exists. If it doesn't, the object has likely not been initialized.
@@ -26,8 +26,8 @@ class TfL_Request:
     def get_line_status(self):
 
         # Check cache for result of request, only request fresh data if cache is empty or older than 60 seconds
-        if self._is_cached(self.tube_status_cache, "tube status"):
-            return self.tube_status_cache
+        if self._is_cached(self._tube_status_cache, "tube status"):
+            return self._tube_status_cache
 
         # Make GET request to TfL API for all status information for tube lines
         r = httpx.get(f"https://api.tfl.gov.uk/Line/Mode/tube/Status?app_key={getenv("TFL_API_KEY")}")
@@ -45,14 +45,14 @@ class TfL_Request:
         line_status["timestamp"] = datetime.now(timezone.utc)
 
         # Cache result for future calls
-        self.tube_status_cache = line_status.copy()
+        self._tube_status_cache = line_status.copy()
         return line_status
     
     def get_tube_closures(self):
 
         # Check cache for result of request, only request fresh data if cache is empty or older than 60 seconds
-        if self._is_cached(self.tube_closures_cache, "tube closures"):
-            return self.tube_closures_cache
+        if self._is_cached(self._tube_closures_cache, "tube closures"):
+            return self._tube_closures_cache
         
          # Make GET request to TfL API for all tube closures
         r = httpx.get(f"https://api.tfl.gov.uk/StopPoint/Mode/tube/Disruption?app_key={getenv("TFL_API_KEY")}")
@@ -83,8 +83,29 @@ class TfL_Request:
         tube_closures["timestamp"] = current_time
 
         # Cache results for future calls
-        self.tube_closures_cache = tube_closures.copy()
+        self._tube_closures_cache = tube_closures.copy()
         return tube_closures
+    
+    def get_station_coords(self, station_id: str):
+
+        # Simplified caching mechanism without time here because this information
+        # does not have an expiration date that is meaningful to this proejct
+        if station_id in self._station_coords_cache:
+            print(f"Cache hit for station {station_id}!")
+            return self._station_coords_cache[station_id]
+        print(f"Cache miss for station {station_id}!")
+        
+        # Make GET request to TfL API for tube station details
+        r = httpx.get(f"https://api.tfl.gov.uk/StopPoint/{station_id}?app_key={getenv("TFL_API_KEY")}")
+        r.raise_for_status()
+
+        station_info = r.json()
+        if "exceptionType" in station_info:
+            raise KeyError(f"Error when retrieving station info for {station_id}. Response code: {station_info["exceptionType"]}. Status: {station_info["httpStatusCode"]} {station_info["httpStatus"]}. URI: {station_info["relativeUri"]}. {station_info["message"]}")
+        
+        coordinates = (station_info["lat"], station_info["lon"])
+        self._station_coords_cache[station_id]= coordinates
+        return coordinates
 
 
 if __name__ == "__main__":
@@ -94,3 +115,6 @@ if __name__ == "__main__":
 
     print(x.get_tube_closures())
     print(x.get_tube_closures())
+
+    print(x.get_station_coords("940GZZLUUXB"))
+    print(x.get_station_coords("940GZZLUUXB"))
